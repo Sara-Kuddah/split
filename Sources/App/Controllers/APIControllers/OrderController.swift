88fdddfe13ce07c1b53ff1b446a3e134f70b2e0b
AlloTheOne <Allo.busy@gmail.com>
2023-05-24 10:44:59 +0300

    routes

--- Sources/App/Controllers/APIControllers/OrderController.swift
+++ Sources/App/Controllers/APIControllers/OrderController.swift
@@ -11,7 +11,7 @@ import Vapor
 
 struct OrderController: RouteCollection {
     func boot(routes: Vapor.RoutesBuilder) throws {
-        <#code#>
+        routes.get("myorders", use: getMeHandler)
     }
     
     // post order / users_orders
@@ -24,26 +24,61 @@ struct OrderController: RouteCollection {
     // post users_orders
     //order_id, user_id, user_type="joined"
     
-    // get order / users_orders *imma start here* -- all my orders--
+    // get order / users_orders *imma start here*
+    // -- all my orders--
     func getMeHandler(req: Request) async throws -> [User_Order] {
         let user = try req.auth.require(User.self)
         let userID = try user.requireID()
         return try await User_Order.query(on: req.db)
+            .with(\.$order)
             .join(Order.self, on: \User_Order.$order.$id == \Order.$id, method: .inner)
             .filter(\.$user.$id == userID)
             .all()
     }
-    // -- active orders --
-    func getActiveOrder(req: Request) async throws -> [User_Order] {
+    
+    
+    // -- all active orders \ for unsigned users --
+    func getActiveOrder(req: Request) async throws -> [Order] {
+        return try await Order.query(on: req.db)
+//            .chunk(max: 10 ){ _ in }
+            .join(User_Order.self, on: \User_Order.$order.$id == \Order.$id, method: .inner)
+            .filter(\Order.$active == true)
+            .sort(Order.self, \.$createdAt)
+            .all()
+//            .range(..<10)
+    
+}
+    
+    // -- my active order --
+    func getMy(req: Request) async throws -> [User_Order] {
         let user = try req.auth.require(User.self)
         let userID = try user.requireID()
+        let orderStatus = try await Order.query(on: req.db).filter(\Order.$active == true).all(\.$active)
         return try await User_Order.query(on: req.db)
+        // -- to get order stuff
+            .with(\.$order)
             .join(Order.self, on: \User_Order.$order.$id == \Order.$id, method: .inner)
-//            .filter(\Order.$active == true)
+        // -- where user_id == signed in, order status not arrived
+            .filter(Order.self, \.$status != "arrived")
+            .filter(Order.self, \.$active == true)
             .filter(\.$user.$id == userID)
+        // -- sort by date
+            .sort(Order.self, \.$createdAt)
+            .all()
+    }
+    
+    
+    
+    // -- all active orders around me --
+    func getActiveAroundMe(req: Request) async throws -> [Order] {
+        let user = try req.auth.require(User.self)
+        let userID = try user.requireID()
+        
+        return try await Order.query(on: req.db)
             
             .all()
     }
+    
     
     // put status
     // put active
