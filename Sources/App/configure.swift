9780fbd24b5675b1026a474376074dd0737da0de
Sara Bin Kuddah <59969962+Sara-Kuddah@users.noreply.github.com>
2023-06-03 22:00:08 +0300

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,9 +15,9 @@ public func configure(_ app: Application) async throws {
         .postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "alaaalabdullah",
+        username: Environment.get("DATABASE_USERNAME") ?? "sarabinkuddah",
         password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "split4"
+        database: Environment.get("DATABASE_NAME") ?? "splie"
     ), as: .psql)
     
     app.migrations.add(CreateUser())
